8eb96bc61292ec7c443d7abaacf376b09c296d92
sasmita1234567 <sasmita.shaurya@gmail.com>
2024-04-28 22:08:17 +0530

    for demo purpose

---
+++ demo.py
@@ -0,0 +1,12 @@
+# Create a Flask web application
+app = Flask(__name__)
+
+# Define a route for the root URL ("/")
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# Run the application if this file is the main program
+if __name__ == '__main__':
+    app.run()
+
